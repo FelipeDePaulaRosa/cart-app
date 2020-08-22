b61a7c90ed7ffae9cc8f9aa276e4332f198b8206
FelipeDePaulaRosa <felipedepaularosa11@gmail.com>
2020-08-22 17:20:34 -0300

    Criando a service da transacao

--- src/main/java/com/cartoes/api/repositories/CartaoRepository.java
+++ src/main/java/com/cartoes/api/repositories/CartaoRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import com.cartoes.api.entities.Cartao;
+import com.cartoes.api.entities.Cliente;
 
 @Transactional(readOnly = true)
 public interface CartaoRepository extends JpaRepository<Cartao, Integer> {
 	@Query("SELECT ca FROM Cartao ca WHERE ca.cliente.id = :clienteId")
 	List<Cartao> findByClienteId(@Param("clienteId") int clienteId);
 	
-	@Query("SELECT ca FROM Cartao ca WHERE ca.numero = :numero")
-	List<Cartao> findByCartaoNumero(@Param("numero") int numero);
+	@Transactional(readOnly = true)
+	Cliente findByNumero(String numero);
 }
--- src/main/java/com/cartoes/api/services/TransacaoService.java
+++ src/main/java/com/cartoes/api/services/TransacaoService.java
@@ -1,5 +1,44 @@
 package com.cartoes.api.services;
 
-public class TransacaoService {
+import java.util.List;
+import java.util.Optional;
 
-}
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import com.cartoes.api.entities.Transacao;
+import com.cartoes.api.repositories.TransacaoRepository;
+import com.cartoes.api.utils.ConsistenciaException;
+
+
+@Service
+public class TransacaoService {
+	private static final Logger log = LoggerFactory.getLogger(TransacaoService.class);
+
+	@Autowired
+	private TransacaoRepository transacaoRepository;
+	/*Verificar as STRINGS corretamente*/
+	public Optional<List<Transacao>> buscarTransacao(int cartaoId) throws ConsistenciaException {
+        log.info("Service: buscando as transacoes do cartao: {}", cartaoId);
+        Optional<List<Transacao>> transacao = Optional.ofNullable(transacaoRepository.findByCartaoId(cartaoId));
+        if (!transacao.isPresent()) {
+            log.info("Service: Nenhuma cartão com id: {} foi encontrado", cartaoId);
+            throw new ConsistenciaException("Nenhuma transacao com o idCartao: {} foi encontrado", cartaoId);
+        }
+        return transacao;
+    }
+
+	public Transacao salvar(Transacao transacao) throws ConsistenciaException {
+		log.info("Service: salvando o cartao: {}", transacao);
+		try {
+			return transacaoRepository.save(transacao);
+		} catch (DataIntegrityViolationException e) {
+			log.info("Service: Já existe um cartão de número {} cadastrado CORRGIR EXCESSAO");
+			throw new ConsistenciaException("Já existe um cartão de número {} cadastrado CORRIGIR EXCESSAO");
+
+		}
+	}
+	
+}	
