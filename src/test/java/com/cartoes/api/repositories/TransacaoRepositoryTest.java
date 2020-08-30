26537bd834f1d9625f5562d2908adf2449dd30bf
FelipeDePaulaRosa <felipedepaularosa11@gmail.com>
2020-08-30 20:15:36 -0300

    TransacaoRepositoryTeste funcionando

--- src/test/java/com/cartoes/api/repositories/TransacaoRepositoryTest.java
+++ src/test/java/com/cartoes/api/repositories/TransacaoRepositoryTest.java
@@ -25,17 +25,38 @@ import com.cartoes.api.entities.Transacao;
 @ActiveProfiles("test")
 public class TransacaoRepositoryTest {
 
-//Criacao da Transação
-	
 	@Autowired
-	private TransacaoRepository transacaoRepository;  
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private CartaoRepository cartaoRepository;
+	@Autowired
+	private TransacaoRepository transacaoRepository;
+
+	private Cliente clienteTeste;
+
 	
-	private Transacao transacaoTeste;	
+	private Cartao cartaoTeste;
+
 	
-	private void CriarTransacaoTestes() throws ParseException {
+	private Transacao transacaoTeste;
+
+	
+	private void CriarTransacaoTestes() throws ParseException {	
+		
+		clienteTeste = new Cliente();
+		clienteTeste.setNome("Nome Teste");
+		clienteTeste.setCpf("05887098082");
+		clienteTeste.setUf("CE");
+		clienteTeste.setDataAtualizacao(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2020"));
+
+		cartaoTeste = new Cartao();
+		cartaoTeste.setNumero("1230981203");
+		cartaoTeste.setDataValidade(new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2020"));
+		cartaoTeste.setBloqueado(false);
+		cartaoTeste.setDataAtualizacao(new SimpleDateFormat("dd/MM/yyyy").parse("01/02/2020"));
+		cartaoTeste.setCliente(clienteTeste);
 		
 		transacaoTeste = new Transacao();
-		
 		transacaoTeste.setDataTransacao(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2020"));
 		transacaoTeste.setCnpj("18808626000194");
 		transacaoTeste.setValor(1500.00);
@@ -44,77 +65,35 @@ public class TransacaoRepositoryTest {
 		transacaoTeste.setCartao(cartaoTeste);
 		
 	}
-//Finalização da transacao.
-	
-//Criacao do Cliente
-	@Autowired
-	private Cliente clienteTeste;
-	
-	private ClienteRepository clienteRepository;
-	
-	private void CriarClienteTestes() throws ParseException {
-		
-		clienteTeste = new Cliente();
-		
-		clienteTeste.setNome("Nome Teste");
-		clienteTeste.setCpf("05887098082");
-		clienteTeste.setUf("CE");
-		clienteTeste.setDataAtualizacao(new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2020"));
-		
-	}
-	
-//Finalização do Cliente.
 
-//Criacao do Cartão
-	@Autowired
-	private Cartao cartaoTeste;
-	
-	private CartaoRepository cartaoRepository;
-	
-	private void CriarCartaoTestes() throws ParseException {
-		
-		cartaoTeste = new Cartao();
-		
-		cartaoTeste.setNumero("1230981203");
-        cartaoTeste.setDataValidade(new SimpleDateFormat("dd/MM/yyyy").parse("01/12/2020"));
-        cartaoTeste.setBloqueado(false);
-        cartaoTeste.setDataAtualizacao(new SimpleDateFormat("dd/MM/yyyy").parse("01/02/2020"));
-        cartaoTeste.setCliente(clienteTeste);
-		
-	}
-	
-//Finalização do Cartão
-	
 	@Before
 	public void setUp() throws Exception {
-		
+
 		CriarTransacaoTestes();
+		clienteRepository.save(clienteTeste);
+		cartaoRepository.save(cartaoTeste);
 		transacaoRepository.save(transacaoTeste);
 		
-		CriarClienteTestes();
-		clienteRepository.save(clienteTeste);
-		
-		CriarCartaoTestes();
-		cartaoRepository.save(cartaoTeste);
-		
 	}
-	
+
 	@Test
-    public void testFindByNumeroCartao() {
+	public void testFindByNumeroCartao() {
 
-		Optional<List<Transacao>> transacoes = transacaoRepository.findByNumeroCartao(transacaoTeste.getCartao().getNumero());
-		Transacao transacao = (Transacao) transacoes.get();
+		Optional<List<Transacao>> transacoes = transacaoRepository
+				.findByNumeroCartao(transacaoTeste.getCartao().getNumero());
 		
-		assertTrue(transacaoTeste.getCartao().getNumero().equals(transacao.getCartao().getNumero()));
+		List<Transacao> transacao = transacoes.get();
+		
+		assertTrue(transacaoTeste.getCartao().getNumero().equals(transacao.get(0).getCartao().getNumero()));
 
-    }
+	}
 
 	@After
 	public void tearDown() throws Exception {
-		
+
 		transacaoRepository.deleteAll();
-		clienteRepository.deleteAll();
 		cartaoRepository.deleteAll();
+		clienteRepository.deleteAll();
 		
 	}
 }
